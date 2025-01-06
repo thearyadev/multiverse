fee5a1354828fd8cc4d96c70d5a8838ec8f25282
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-01-06 14:02:28 -0500

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/app/header";
+import ScreenSizeIndicator from "@/components/app/ssi";
+import Image from "next/image";
+import { SewingPinFilledIcon } from "@radix-ui/react-icons";
+import { MailIcon, PhoneIcon, MapPinIcon } from "lucide-react";
+import { SocialIcon } from "react-social-icons";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +33,56 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
+        <ScreenSizeIndicator />
         {children}
+        <footer className="w-full bg-zinc-950 px-3 md:px-8 lg:px-28 flex flex-wrap justify-center space-x-8">
+          <Image
+            className="w-full sm:w-auto"
+            src="/stacked_logo.png"
+            alt="Logo"
+            width={300}
+            height={300}
+          />
+          <div className="text-white">
+            <div className="py-8 flex flex-col space-y-3">
+              <h3 className="text-2xl font-extrabold py-3 px-2">Get In Touch</h3>
+              <div className="flex items-center">
+                <MapPinIcon className="h-8 w-8 pr-2" />
+                <p className="">
+                  100 Consilium Place, Suite 200 Toronto, ON M1H 3E3
+                </p>
+              </div>
+
+              <div className="flex items-center">
+                <MailIcon className="h-8 w-8 pr-2" />
+                <p className="">
+                  info@multiverseimmigration.ca
+                </p>
+              </div>
+              <div className="flex items-center">
+                <PhoneIcon className="h-8 w-8 pr-2" />
+                <p className="">
+                  +1 (416) 587-5348
+                </p>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="py-8 flex space-x-4">
+            <SocialIcon url="https://twitter.com" />
+            <SocialIcon url="https://facebook.com" />
+            <SocialIcon url="https://linkedin.com" />
+            <SocialIcon url="https://instagram.com" />
+
+
+
+          </div>
+          <div className="w-full text-white text-center py-2 text-sm">
+            2024 Multiverse Immigration Consultancy Inc. All rights reserved.
+          </div>
+        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,171 @@
-import Image from "next/image";
+import { CtaBanner } from "@/components/app/ctaBanner";
+import { Hero } from "@/components/app/hero";
+import { IconCard } from "@/components/app/icon-card";
+import { ProgramCard } from "@/components/app/program-card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import Link from "next/link";
+import { Plus, Briefcase, Book, Store, ArchiveRestore, Users } from "lucide-react";
 
-export default function Home() {
+const pathways = [
+  {
+    name: "Work Permit",
+    description: "Navigate the complexities of obtaining a work permit with our comprehensive support and resources.",
+    imageSrc: "/pathways/work.jpg",
+    href: "/work",
+    icon: Briefcase
+  },
+  {
+    name: "Study Permit",
+    description: "Personalized assistance for securing study permits, helping you achieve your educational dreams.",
+    imageSrc: "/pathways/study.jpg",
+    href: "/study",
+    icon: Book
+  },
+  {
+    name: "Business Immigration",
+    description: "Consulting for businesses owners looking to expand their operations internationally.",
+    imageSrc: "/pathways/business.jpeg",
+    href: "/business",
+    icon: Store
+  },
+  {
+    name: "Start-Up Visa",
+    description: "Navigate the complexities of obtaining a work permit with our comprehensive support and resources.",
+    imageSrc: "/pathways/start-up.jpg",
+    href: "/start-up",
+    icon: ArchiveRestore
+  },
+  {
+    name: "Family Sponsorship",
+    description: "Navigate the complexities of obtaining a work permit with our comprehensive support and resources.",
+    imageSrc: "/pathways/family.jpg",
+    href: "/family",
+    icon: Users
+  },
+  {
+    name: "Express Entry",
+    description: "Navigate the complexities of obtaining a work permit with our comprehensive support and resources.",
+    imageSrc: "/pathways/express.jpg",
+    href: "/family",
+    icon: Users
+  }
+]
+
+export default function Index() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Hero src="/hero.jpeg" />
+      <CtaBanner href="/evaluation" />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="py-12">
+        <h1 className="text-4xl lg:text-6xl font-thin text-center">Immigration Pathways</h1>
+        <div className=" py-8 flex justify-center">
+          <hr className=" border-gray-300 w-10/12" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-14 px-5">
+          {pathways.map((pathway) => (
+            <IconCard
+              key={pathway.name}
+              icon={pathway.icon}
+              title={pathway.name}
+              description={pathway.description}
+              buttonText="Learn More"
+              href={pathway.href}
+              imageSrc={pathway.imageSrc}
+            />
+          ))}
+
+        </div>
+      </div>
+
+      <div className=" py-8 flex justify-center">
+        <hr className=" border-gray-300 w-10/12" />
+      </div>
+
+      <div className="py-12 flex justify-center">
+        <div className="w-5/6 md:w-3/4 lg:w-1/2 xl:1-1/3 2xl:w-1/3">
+          <h2 className="text-2xl font-semibold text-center">Frequently Asked Questions</h2>
+          <Accordion type="multiple">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>What are the immigration programs can I apply for?</AccordionTrigger>
+              <AccordionContent>
+                You may be able to immigrate to Canada under several programs. They may be broadly categorized under Federal Programs and Provincial Nominee Programs.
+
+                Under the Federal Express Entry Pathway, you can immigrate under the Federal Skilled Worker, Federal Skilled Trade & Canadian Experience Class. You may also immigrate as a student on a Study Permit or as a worker on a Work Permit, Caregiver Pilot Program, Self-Employed and Start-up Visa Program.
+
+                In addition to the Federal Programs, you may also qualify under the Provincial Nominees Programs, which vary from province to province.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>
+                How long does it take to process my application?
+              </AccordionTrigger>
+              <AccordionContent>
+                Processing time depends on several factors like the application type, country of application as well as how the application was done, i.e., online or paper. Visit <a className="text-blue underline" target="_blank" href="https://ircc.canada.ca/english/information/fees/fees.asp">ircc.canada.ca</a> for more information.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>What are processing fees?</AccordionTrigger>
+              <AccordionContent>
+                Most applications include processing fees. Exact processing fees may vary depending on the type of the application, among other factors. Visit <a className="text-blue underline" target="_blank" href="https://www.canada.ca/en/immigration-refugees-citizenship/services/application/check-processing-times.html">canada.ca</a> for more information.
+
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+              <AccordionTrigger>How can I hire a foreign worker?</AccordionTrigger>
+              <AccordionContent>
+                Canadian employers can hire foreign workers to fill temporary jobs when qualified Canadians are not available under The Temporary Foreign Worker (TFW) Program. This program is regulated through the Immigration and Refugee Protection Act and the Immigration and Refugee Protection Regulations and is administered in partnership with Immigration, Refugees and Citizenship Canada (IRCC) and the Canada Border Services Agency (CBSA). Learn more <a href="/employers" className="text-blue underline">here</a>
+
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+          <p className="pt-5 text-center text-sm">More Questions? <Link className="hover:underline italic" href="/contact">Contact Us</Link></p>
+        </div>
+      </div>
+
+    </>
   );
 }
+
+// <ProgramCard
+//   programName="Work Permit"
+//   programDiscription="Navigate the complexities of obtaining a work permit with our comprehensive support and resources."
+//   programImageSrc="/vacation.png"
+//   programLink="/work"
+// />
+// <ProgramCard
+//   programName="Study Permit"
+//   programDiscription="Personalized assistance for securing study permits, helping you achieve your educational dreams."
+//   programImageSrc="/vacation.png"
+//   programLink="/study"
+// />
+// <ProgramCard
+//   programName="Business Immigration"
+//   programDiscription="Consulting for businesses owners looking to expand their operations internationally."
+//   programImageSrc="/vacation.png"
+//   programLink="/business"
+// />
+// <ProgramCard
+//   programName="Work Permit"
+//   programDiscription="Navigate the complexities of obtaining a work permit with our comprehensive support and resources."
+//   programImageSrc="/vacation.png"
+//   programLink="/work"
+// />
+// <ProgramCard
+//   programName="Study Permit"
+//   programDiscription="Personalized assistance for securing study permits, helping you achieve your educational dreams."
+//   programImageSrc="/vacation.png"
+//   programLink="/study"
+// />
+// <ProgramCard
+//   programName="Business Immigration"
+//   programDiscription="Consulting for businesses owners looking to expand their operations internationally."
+//   programImageSrc="/vacation.png"
+//   programLink="/business"
+// />
