2ec4854f542f93f1526ff823c4bad29e30da8789
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-01-06 21:38:49 -0500

    add: favicon

--- app/(articles)/layout.tsx
+++ app/(articles)/layout.tsx
@@ -1,7 +1,13 @@
+import { CtaBanner } from "@/components/app/ctaBanner";
+import Link from "next/link";
 export default function ArticleLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="py-8 px-4 max-w-4xl mx-auto">
-      {children}
-    </div>
+    <>
+      <div className="py-8 px-4 max-w-4xl mx-auto">
+        {children}
+      </div>
+      <p className="py-5 text-center text-sm">More Questions? <Link className="hover:underline italic" href="/contact">Contact Us</Link></p>
+      <CtaBanner href="/evaluation" />
+    </>
   )
 }
