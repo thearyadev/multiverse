c296a7dd1835f7f78cef4cd5bf34a45ab798a675
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-01-07 00:35:43 -0500

    fix: social button alignment

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,44 +25,49 @@ export default function RootLayout({
         <Header />
         <ScreenSizeIndicator />
         {children}
-        <footer className="w-full bg-zinc-950 px-3 md:px-8 lg:px-28 flex flex-wrap justify-center md:space-x-8">
-          <div className="w-full md:w-auto flex justify-center">
-            <Image
-              className="w-96"
-              src="/stacked_logo.png"
-              alt="Logo"
-              width={300}
-              height={300}
-            />
-          </div>
+        <footer className="bg-zinc-950 text-white">
+          <div className="w-full px-3 md:px-8 lg:px-28 flex flex-wrap justify-center md:space-x-8">
 
-          <div className="text-white">
-            <div className="py-8 flex flex-col space-y-3">
-              <h3 className="text-2xl font-extrabold py-3 px-2">Get In Touch</h3>
-              <div className="flex items-center">
-                <MapPinIcon className="h-8 w-8 pr-2" />
-                <p className="">
-                  100 Consilium Place, Suite 200 Toronto, ON M1H 3E3
-                </p>
-              </div>
-
-              <div className="flex items-center">
-                <MailIcon className="h-8 w-8 pr-2" />
-                <p className="">
-                  info@multiverseimmigration.ca
-                </p>
-              </div>
-              <div className="flex items-center">
-                <PhoneIcon className="h-8 w-8 pr-2" />
-                <p className="">
-                  +1 (416) 587-5348
-                </p>
-              </div>
+            <div className="w-full md:w-auto flex justify-center">
+              <Image
+                className="w-96"
+                src="/stacked_logo.png"
+                alt="Logo"
+                width={300}
+                height={300}
+              />
             </div>
 
+            <div className="text-white">
+              <div className="py-8 flex flex-col space-y-3">
+                <h3 className="text-2xl font-extrabold py-3 px-2">Get In Touch</h3>
+                <div className="flex items-center">
+                  <MapPinIcon className="h-8 w-8 pr-2" />
+                  <p className="">
+                    100 Consilium Place, Suite 200 Toronto, ON M1H 3E3
+                  </p>
+                </div>
+
+                <div className="flex items-center">
+                  <MailIcon className="h-8 w-8 pr-2" />
+                  <p className="">
+                    info@multiverseimmigration.ca
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <PhoneIcon className="h-8 w-8 pr-2" />
+                  <p className="">
+                    +1 (416) 587-5348
+                  </p>
+                </div>
+              </div>
+
+
+            </div>
 
           </div>
-          <div className="py-8 grid grid-cols-4 gap-4">
+
+          <div className="flex flex-row justify-center space-x-4 py-4">
             <SocialIcon url="https://twitter.com" />
             <SocialIcon url="https://facebook.com" />
             <SocialIcon url="https://linkedin.com" />
