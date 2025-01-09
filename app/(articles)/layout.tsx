17eca3851540760551ac03d5b17ed0f3c05de7d6
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2025-01-09 15:32:04 -0500

    add: articles

--- app/(articles)/layout.tsx
+++ app/(articles)/layout.tsx
@@ -6,7 +6,7 @@ export default function ArticleLayout({ children }: { children: React.ReactNode 
       <div className="py-8 px-4 max-w-4xl mx-auto">
         {children}
       </div>
-      <p className="py-5 text-center text-sm">More Questions? <Link className="hover:underline italic" href="/contact">Contact Us</Link></p>
+      <p className="py-5 text-center text-sm">More Questions? <Link className="hover:underline italic" href="mailto:info@multiverseimmigration.ca">Contact Us</Link></p>
       <CtaBanner href="/evaluation" />
     </>
   )
